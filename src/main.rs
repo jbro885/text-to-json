efad127981ecd1a76be1c54b31d90518ea8780e2
adiatma <adiatma.kamaarudin@ralali.com>
2019-10-24 23:30:31 +0700

    docs: update readme

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate text_to_json;
 use text_to_json::read;
 
 fn main() {
+  // Example: to read::to_json(file.txt).
   match read::to_json(String::from("example.txt")) {
     Ok(n) => println!("{:?}", n),
     Err(_) => panic!("Error"),
